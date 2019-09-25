4a987aafb3b4220d83d76ce2c7b891d0025ac31d
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-09-25 13:01:23 +0200

    experiment more

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,22 @@ module.exports = {
             { text: 'Home', link: '/' },
             { text: 'Guide', link: '/guide/' },
             { text: 'External', link: 'https://google.com' }
+        ],
+        smoothScroll: true,
+        sidebar: [
+            {
+                title: 'Group 1',   // required
+                path: '/foo/',      // optional, which should be a absolute path.
+                collapsable: false, // optional, defaults to true
+                sidebarDepth: 1,    // optional, defaults to 1
+                children: [
+                '/'
+                ]
+            },
+            {
+                title: 'Group 2',
+                children: [ /* ... */ ]
+            }
         ]
     }
 }
