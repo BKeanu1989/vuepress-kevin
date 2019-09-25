701fd01681adafacb600d48068dc1142075e046a
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-09-25 12:45:59 +0200

    experiment more

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
+    title: 'Kevin Fechner',
+    description: 'Interesting and not interesting things from Kevin Fechner',
     configureWebpack: {
         resolve: {
             alias: {
             '@alias': 'path/to/some/dir'
             }
         }
+    },
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' }
+        ]
     }
 }
