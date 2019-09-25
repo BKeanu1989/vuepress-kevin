af090fbe4c4c51c15a2e4329538abe4649c0e906
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-09-25 18:44:43 +0200

    start to add i18n

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,5 +30,29 @@ module.exports = {
                 children: [ /* ... */ ]
             }
         ]
+    },
+    locales: {
+        // The key is the path for the locale to be nested under.
+        // As a special case, the default locale can use '/' as its path.
+        '/': {
+            lang: 'en-US', // this will be set as the lang attribute on <html>
+            title: 'VuePress',
+            description: 'Vue-powered Static Site Generator',
+            selectText: 'Languages',
+            // label for this locale in the language dropdown
+            label: 'English',
+            // Aria Label for locale in the dropdown
+            ariaLabel: 'Languages'
+        },
+        '/de/': {
+            lang: 'de-DE',
+            title: 'VuePress',
+            description: 'Interessantes und uninteressantes von Kevin Fechner',
+            selectText: 'Sprachen',
+            // label for this locale in the language dropdown
+            label: 'Deutsch',
+            // Aria Label for locale in the dropdown
+            ariaLabel: 'Sprachen'
+        }
     }
 }
